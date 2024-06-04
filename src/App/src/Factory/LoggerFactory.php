707731f1476a6e0cd78c40c439150cf30a5fd3a3
Andy Bleakley <ajbleakley@hotmail.com>
2024-06-04 09:33:30 +0100

    Name default logging service and set the stream path to use today's date as the filename for the log
    Should help distribute logs across multiple files and make logs easier to find. In the future we could add functionality to periodically remove old log files.

--- src/App/src/Factory/LoggerFactory.php
+++ src/App/src/Factory/LoggerFactory.php
@@ -4,20 +4,27 @@ declare(strict_types=1);
 
 namespace App\Factory;
 
+use DateTimeImmutable;
 use Laminas\ServiceManager\Factory\FactoryInterface;
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 use Psr\Container\ContainerInterface;
 
+use function sprintf;
+
 class LoggerFactory implements FactoryInterface
 {
+    private const STREAM_PATH = 'data/log/%s.log';
+
     /**
      * @inheritDoc
      */
     public function __invoke(ContainerInterface $container, $requestedName, ?array $options = null)
     {
-        $logger = new Logger('name');
-        $logger->pushHandler(new StreamHandler('data/log/test.log'));
+        $logger = new Logger('default');
+        $logger->pushHandler(
+            new StreamHandler(sprintf(self::STREAM_PATH, (new DateTimeImmutable())->format('Ymd')))
+        );
         return $logger;
     }
 }
