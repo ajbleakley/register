26f7329751c29aa36553aa328fd397d921a6f268
Andy Bleakley <ajbleakley@hotmail.com>
2024-06-04 09:02:22 +0100

    Add a basic usage example of logging to the ping handler
    Example matches the basic usage example given on the official Monolog repo: https://github.com/Seldaek/monolog
    
    To test, run the application using PHP's built-in web server: `composer serve`
    
    Then navigate to: http://0.0.0.0:8080/api/ping
    
    A warning and error message will be logged to `data/log/test.log`

--- src/App/src/Handler/PingHandler.php
+++ src/App/src/Handler/PingHandler.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace App\Handler;
 
 use Laminas\Diactoros\Response\JsonResponse;
+use Monolog\Handler\StreamHandler;
+use Monolog\Level;
+use Monolog\Logger;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
@@ -15,6 +18,14 @@ class PingHandler implements RequestHandlerInterface
 {
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
+        // create a log channel
+        $log = new Logger('name');
+        $log->pushHandler(new StreamHandler('data/log/test.log', Level::Warning));
+
+        // add records to the log
+        $log->warning('Foo');
+        $log->error('Bar');
+
         return new JsonResponse(['ack' => time()]);
     }
 }
