6af80bbe24008e9d628d13dadab402773c4c5364
Andy Bleakley <ajbleakley@hotmail.com>
2024-06-10 11:46:03 +0100

    When a user is successfully created, redirect the request to fetch the user
    The action to fetch a user responds with structured Hypertext Application Language (HAL).

--- test/AppTest/ADR/Action/API/CreateUserActionTest.php
+++ test/AppTest/ADR/Action/API/CreateUserActionTest.php
@@ -8,16 +8,17 @@ use App\ADR\Action\API\CreateUserAction;
 use App\ADR\Domain\CreateUser\CreateUserDomain;
 use App\ADR\Domain\CreateUser\UserCreatedDomainResult;
 use App\ADR\Domain\CreateUser\UserNotCreatedDomainResult;
-use App\ADR\Responder\API\JsonResponder;
+use Mezzio\Helper\UrlHelper;
 use PHPUnit\Framework\TestCase;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use UnexpectedValueException;
 
 class CreateUserActionTest extends TestCase
 {
     // dependencies
     private CreateUserDomain $domain;
-    private JsonResponder $responder;
+    private UrlHelper $urlHelper;
 
     // parameters
     private ServerRequestInterface $request;
@@ -26,7 +27,7 @@ class CreateUserActionTest extends TestCase
     {
         // dependencies
         $this->domain    = $this->createMock(CreateUserDomain::class);
-        $this->responder = new JsonResponder();
+        $this->urlHelper = $this->createMock(UrlHelper::class);
 
         // parameters
         $this->request = $this->createMock(ServerRequestInterface::class);
@@ -34,10 +35,10 @@ class CreateUserActionTest extends TestCase
 
     private function sut(): ResponseInterface // subject under test
     {
-        return (new CreateUserAction($this->domain, $this->responder))->handle($this->request);
+        return (new CreateUserAction($this->domain, $this->urlHelper))->handle($this->request);
     }
 
-    public function testWhenUserCreatedThenRespondWithSuccessResponse(): void
+    public function testWhenUserCreatedThenRespondWithRedirectResponse(): void
     {
         // given
         $this->request->method('getParsedBody')->willReturn([
@@ -51,33 +52,33 @@ class CreateUserActionTest extends TestCase
         $response = $this->sut();
 
         // then
-        self::assertEquals(200, $response->getStatusCode());
+        self::assertEquals(302, $response->getStatusCode());
     }
 
-    public function testWhenNotReadyToCreateUserThenRespondWithFailureResponse(): void
+    public function testWhenNotReadyToCreateUserThenActionExitsEarly(): void
     {
         // given
         $this->request->method('getParsedBody')->willReturn([
             // missing required body
         ]);
 
-        // when
-        $response = $this->sut();
-
         // then
-        self::assertEquals(400, $response->getStatusCode());
+        $this->expectException(UnexpectedValueException::class);
+
+        // when
+        $this->sut();
     }
 
-    public function testWhenUserCreationFailsThenRespondWithFailureResponse(): void
+    public function testWhenUserCreationFailsThenThenActionExitsEarly(): void
     {
         // given
         $this->domain->method('process')
             ->willReturn($this->createMock(UserNotCreatedDomainResult::class));
 
-        // when
-        $response = $this->sut();
-
         // then
-        self::assertEquals(400, $response->getStatusCode());
+        $this->expectException(UnexpectedValueException::class);
+
+        // when
+        $this->sut();
     }
 }
